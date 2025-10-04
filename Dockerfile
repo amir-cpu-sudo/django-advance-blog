cce0c741dffc9980c3666b4cf117a62dd67b9ab1
amir-cpu-sudo <amirmohammadranjbar30@gmail.com>
2025-10-04 12:48:45 +0330

    adding secret key to docker compose

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ WORKDIR /app
 COPY ./requirements.txt /app/
 
 RUN pip3 install -r requirements.txt
+RUN pip3 install python-decouple
 
 COPY ./core /app
 
--- core/core/settings.py
+++ core/core/settings.py
@@ -18,14 +18,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+from decouple import config
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@)zft4u0hamu7ia@8r1nbbyt8x500r5@6nv^!jd+@s&p-4xbh5'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')],default="*")
 
 
 # Application definition
