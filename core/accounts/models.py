adca4ea897d5e89a4a5f97d94820908ae6a71a80
amir-cpu-sudo <amirmohammadranjbar30@gmail.com>
2025-10-06 12:32:21 +0330

    add singals to project

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,PermissionsMixin)
 from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -59,3 +61,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.email
+    
+
+@receiver(post_save, sender=User)
+def save_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
